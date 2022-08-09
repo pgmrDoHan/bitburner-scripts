9711f3f723295dcf363994ba39d60dc53b9fa4bf
prgmrDoHan <andong_kdh@naver.com>
2022-08-10 01:00:53 +0900

    update

--- hackingTool.js
+++ hackingTool.js
@@ -4,14 +4,19 @@ import {
 } from './scriptLib';
 
 export async function main(ns) {
-	let selectedServer = await scrapArgs(ns, "-S");
-	if (selectedServer === -1) {
-		const serverList = ns.scan(ns.getHostname());
-		selectedServer = await ns.prompt("Which Server to want to hack?", {
-			type: "select",
-			choices: serverList
-		});
-	}
+	let selectedHost = await scrapArgs(ns, "-H");
+    if (selectedHost === -1) {
+        selectedHost = ns.getHostname();
+    }
+
+    let selectedServer = await scrapArgs(ns, "-S");
+    if (selectedServer === -1) {
+        const serverList = ns.scan(selectedHost);
+        selectedServer = await ns.prompt("Which Server to want to hack?", {
+            type: "select",
+            choices: serverList
+        });
+    }
 	while (true) {
 		if (ns.hasRootAccess(selectedServer) !== false) {
 			await serverHacking(ns, selectedServer);
